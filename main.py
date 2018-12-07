c6f26e1cb721152f4d6eae695fec413bc8e9bad3
Joke De Winter <joke@jokedewinter.co.uk>
2018-12-07 08:37:42 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from random import randint
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	# return name
+	quotes = ["If people do not believe that mathematics is simple, it Neumann",
+			"Computer science is no more about computers than astronomy",
+			"To understand recursion you must first understand recursion", 
+			"You look at things that are and ask, why? I dream of things",
+			"Mathematics is the key and door to the sciences. -- Galileo",
+			"Not everyone will understand your journey. Thats fine."]
+	randomNumber = randint(0,len(quotes)-1)
+	quote = quotes[randomNumber]    
+	return render_template("index.html", title="home", **locals())
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="about")
+
+@app.route("/confirmation", methods=["POST"])
+def confirmation():
+	form_data = request.form
+	print (form_data["email"])
+	#return "All OK"
+	return render_template("confirmation.html", title="Form confirmation", result="All OK")
+
+if __name__ == "__main__":
+    app.run(debug=True)
