85c7f58edacf8a968b649265b07c89760209e339
Joke De Winter <joke@jokedewinter.co.uk>
2019-01-17 13:21:49 +0000

    add line in procfile

--- app.py
+++ app.py
@@ -17,9 +17,11 @@ def index():
 		
 	return render_template("index.html", title="home", **locals())
 
+
 @app.route("/about")
 def about():
    	return render_template("about.html", title="about")        
+
 
 @app.route("/confirmation", methods=["POST"])
 def confirmation():
