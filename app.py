4e5c9c8702a6ba36d543f77769b4ed1706d9ed66
Joke De Winter <joke@jokedewinter.co.uk>
2019-01-17 21:27:32 +0000

    fixing indentation error

--- app.py
+++ app.py
@@ -5,31 +5,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	# return name
-    quotes = ["If people do not believe that mathematics is simple, it Neumann",
-	"Computer science is no more about computers than astronomy",
-	          "To understand recursion you must first understand recursion",
-	          "You look at things that are and ask, why? I dream of things",
-	          "Mathematics is the key and door to the sciences. -- Galileo",
-	          "Not everyone will understand your journey. That's fine."]
-	random_number = randint(0, len(quotes)-1)
-	quote = quotes[random_number]
-		
-	return render_template("index.html", title="home", **locals())
+    quotes = ["If people do not believe that mathematics is simple, it Neumann", "Computer science is no more about computers than astronomy", "To understand recursion you must first understand recursion", "You look at things that are and ask, why? I dream of things", "Mathematics is the key and door to the sciences. -- Galileo", "Not everyone will understand your journey. That's fine."]
+    random_number = randint(0, len(quotes) - 1)
+    quote = quotes[random_number]
+    return render_template("index.html", title="home", **locals())
 
 
 @app.route("/about")
 def about():
-	return render_template("about.html", title="about")
+    return render_template("about.html", title="about")
 
 
 @app.route("/confirmation", methods=["POST"])
 def confirmation():
-	form_data = request.form
-	email = form_data["email"]
-	result="All OK"
-	return render_template("confirmation.html", title="Form confirmation", **locals())
+    form_data = request.form
+    email = form_data["email"]
+    result="All OK"
+    return render_template("confirmation.html", title="Form confirmation", **locals())
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
